a945b64ec2c8d6a88aed734ddc59f5521bcab5eb
silverwind <me@silverwind.io>
2025-08-01 12:46:53 +0200

    rename type Options to CidrRegexOptions

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import ipRegex from "ip-regex";
 
-type Options = {
+type CidrRegexOptions = {
   /**
   Only match an exact string. Useful with `RegExp#test()` to check if a string is an IP address. *(`false` matches any IP address in a string)*
 
@@ -9,7 +9,7 @@ type Options = {
   readonly exact?: boolean;
 }
 
-const defaultOpts: Options = {exact: false};
+const defaultOpts: CidrRegexOptions = {exact: false};
 const v4str = `${ipRegex.v4().source}\\/(3[0-2]|[12]?[0-9])`;
 const v6str = `${ipRegex.v6().source}\\/(12[0-8]|1[01][0-9]|[1-9]?[0-9])`;
 
@@ -18,7 +18,7 @@ const v4exact = new RegExp(`^${v4str}$`);
 const v6exact = new RegExp(`^${v6str}$`);
 const v46exact = new RegExp(`(?:^${v4str}$)|(?:^${v6str}$)`);
 
-const cidrRegex = ({exact}: Options = defaultOpts) => exact ? v46exact : new RegExp(`(?:${v4str})|(?:${v6str})`, "g");
-export const v4 = cidrRegex.v4 = ({exact}: Options = defaultOpts) => exact ? v4exact : new RegExp(v4str, "g");
-export const v6 = cidrRegex.v6 = ({exact}: Options = defaultOpts) => exact ? v6exact : new RegExp(v6str, "g");
+const cidrRegex = ({exact}: CidrRegexOptions = defaultOpts) => exact ? v46exact : new RegExp(`(?:${v4str})|(?:${v6str})`, "g");
+export const v4 = cidrRegex.v4 = ({exact}: CidrRegexOptions = defaultOpts) => exact ? v4exact : new RegExp(v4str, "g");
+export const v6 = cidrRegex.v6 = ({exact}: CidrRegexOptions = defaultOpts) => exact ? v6exact : new RegExp(v6str, "g");
 export default cidrRegex;
